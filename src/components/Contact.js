ca3f48daadb31d5832af72882b36df3a277df46b
cbeisel <collin.beisel@toyota.com>
2022-03-16 11:11:31 -0500

    updated about me paragraph

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,9 @@ import 'react-bootstrap';
 
 export const Contact = () => {
   return (
-    <div className='mx-auto'>
+    <div className='mx-auto contact-form'>
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf-M_GxTr1gn-Sw1aF0ViOvUiY280_VpGAheUUjIiueLOH8tA/viewform?embedded=true" 
-        width="640" height="844" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        >Loading…</iframe>
     </div>
   )
 }
